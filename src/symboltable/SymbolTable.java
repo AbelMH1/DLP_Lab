b05983dd172e9f06875870fe73ea2508a8f13d62
Abel <>
2024-03-27 01:35:09 +0100

    Añadida documentación a los métodos de SymbolTable

--- src/symboltable/SymbolTable.java
+++ src/symboltable/SymbolTable.java
@@ -12,16 +12,27 @@ public class SymbolTable {
 		table.add(new HashMap<>());
 	}
 
+	/**
+	 * Aumenta el ámbito en que se guardarán las definiciones siguientes a uno más concreto.
+	 */
 	public void set() {
 		table.add(new HashMap<>());
 		scope++;
 	}
-	
+
+	/**
+	 * Reduce el ámbito en que se guardarán las definiciones siguientes a uno más amplio.
+	 */
 	public void reset() {
 		table.remove(scope);
 		scope--;
 	}
-	
+
+	/**
+	 * Añade la definición recibida al ámbito actual.
+	 * @param definition La definición a añadir a la Tabla de Símbolos.
+	 * @return True si se añade correctamente, False si ya existe una definición con el mismo nombre en dicho ámbito.
+	 */
 	public boolean insert(Definition definition) {
 		Map<String,Definition> st = table.get(scope);
 		boolean isPresent = st.containsKey(definition.getName());
@@ -31,7 +42,13 @@ public class SymbolTable {
 		}
 		return !isPresent;
 	}
-	
+
+	/**
+	 * Busca la definición con el nombre recibido en el ámbito actual. Si no la encuentra busca en el ámbito superior
+	 * y así hasta que la encuentre o se acaben los ámbitos.
+	 * @param id El nombre de la definición a encontrar a la Tabla de Símbolos.
+	 * @return La Definición si la encuentra, Null en caso contrario.
+	 */
 	public Definition find(String id) {
 		Definition def = null;
 		for (int i = scope; def == null && i >= 0 ; i--)
@@ -39,6 +56,11 @@ public class SymbolTable {
 		return def;
 	}
 
+	/**
+	 * Busca la definición con el nombre recibido exclusivamente en el ámbito actual.
+	 * @param id El nombre de la definición a encontrar a la Tabla de Símbolos.
+	 * @return La Definición si la encuentra, Null en caso contrario.
+	 */
 	public Definition findInCurrentScope(String id) {
 		return table.get(scope).get(id);
 	}
