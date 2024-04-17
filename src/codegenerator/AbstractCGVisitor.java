f9492503bde40f7ba2020ca50ed845b600298408
Abel <abelmhov7l@gmail.com>
2024-04-17 21:43:09 +0200

    Añadida información adicional a las excepciones del AbstractCGVisitor

--- src/codegenerator/AbstractCGVisitor.java
+++ src/codegenerator/AbstractCGVisitor.java
@@ -13,156 +13,156 @@ public class AbstractCGVisitor<TP,TR> implements Visitor<TP,TR> {
 
     @Override
     public TR visit(Program e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(RecordField e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(FunctionDefinition e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(VariableDefinition e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Arithmetic e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(ArrayAccess e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Cast e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(CharLiteral e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Comparison e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(DoubleLiteral e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(IntLiteral e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Logical e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(LogicalNot e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(StructAccess e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(UnaryMinus e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Variable e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Assignment e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(FunctionInvocation e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(If e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Input e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Print e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(Return e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(While e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(ErrorType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(ArrayType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(CharType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(DoubleType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(IntType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(StructType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(VoidType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 
     @Override
     public TR visit(FunctionType e, TP param) {
-        throw new IllegalStateException();
+        throw new IllegalStateException("Invocación inválida en: " + e.getClass().getName());
     }
 }
